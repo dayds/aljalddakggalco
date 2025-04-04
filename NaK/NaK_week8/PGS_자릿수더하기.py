6ba7e2d2c6b59921acf02535d8cb8a08556cdcab
NKyeong <150774509+NKyeong@users.noreply.github.com>
2025-04-04 19:21:17 +0900

    Revert "[DSCT-43] GitHub PR 2주차"

--- NaK/NaK_week8/PGS_자릿수더하기.py
+++
@@ -1,5 +0,0 @@
-def solution(n):
-    answer = 0
-    for i in list(str(n)):
-        answer += int(i)
-    return answer
--- NaK/NaK_week9/PGS_제곱수판별하기.py
+++
@@ -1,2 +0,0 @@
-def solution(n):
-    return 1 if (n ** (1/2)) % 1 == 0 else 2
